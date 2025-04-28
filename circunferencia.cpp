3cbf0930e0f66c26f6cdbb3c202fbeadb8777136
vane-rios1 <riosperezvanessa824@gmail.com>
2025-04-28 16:50:55 -0600

    Update circunferencia.cpp

--- circunferencia.cpp
+++ circunferencia.cpp
@@ -9,7 +9,7 @@ int main() {
     cout << "Ingrese el radio del circulo: ";
     cin >> radio;
 
-    circunferencia = 3.1416 * (radio + radio); // O también se podría usar 2 * PI * radio
+    circunferencia = 2 * 3.1416 * radio;
 
     cout << "El radio del circulo es de " << radio << ", el perimetro de la circunferencia es de " << circunferencia << endl;
 
